e01296d2f559027f13f845fd5b1f2ce63350eeaf
MudassarGill <mudassarhussain6533@gmail.com>
2026-03-26 21:25:44 -0700

    vector adde

--- scalars/pratice.py
+++ scalars/pratice.py
@@ -14,3 +14,24 @@ print(f'power {a**b}')
 print(f'modulo {a%b}') 
 
 print(f'floor division {a//b}') 
+
+
+
+
+
+temp=500
+
+print(f'temp in celcius {temp-32/1.8}')
+
+
+
+
+v1=[23,4,5]
+v2=[4,5,6]
+
+result=[]
+
+for i in range(len(v1)):
+    result.append(v1[i]+v2[i])
+
+print(result)  #vector
