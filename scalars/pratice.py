363d0cc6e8a70472818cc540d7950ea28d256134
MudassarGill <mudassarhussain6533@gmail.com>
2026-03-26 21:20:42 -0700

    basic maths opreations

---
+++ scalars/pratice.py
@@ -0,0 +1,16 @@
+a=10
+b=4
+
+print(f'Addation {a+b}')  
+
+print(f'subtraction {a-b}')  
+
+print(f'multiplication {a*b}')  
+
+print(f'division {a/b}')  
+
+print(f'power {a**b}')  
+
+print(f'modulo {a%b}') 
+
+print(f'floor division {a//b}') 
