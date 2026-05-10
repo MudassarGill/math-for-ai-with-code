ba23cfc8a47d35bd6abd19a6b09d8ae7432c929b
MudassarGill <mudassarhussain6533@gmail.com>
2026-05-09 21:18:08 -0700

    workflow run successfully

--- revisePython/webhook.py
+++ revisePython/webhook.py
@@ -1,8 +1,9 @@
 import requests
-user_message='Hi how are you ?'
+user_message='what is the captical of pakistan?'
 request_message={'message':user_message}
 
 
 url="http://localhost:5678/webhook-test/5d159401-26b7-440b-b3bf-e578bdb99714"
 response=requests.post(url,json=request_message)
 print(response.status_code)
+print(response.json())
