b60ec0724eca58e489a2d37dd21e702e91589ce8
MudassarGill <mudassarhussain6533@gmail.com>
2026-05-16 20:46:57 -0700

    function added

--- revisePython/webhook.py
+++ revisePython/webhook.py
@@ -1,9 +1,9 @@
 import requests
-user_message='what is the captical of pakistan?'
+user_message='who is the current perisent of pakistan?'
 request_message={'message':user_message}
 
 
 url="http://localhost:5678/webhook-test/5d159401-26b7-440b-b3bf-e578bdb99714"
 response=requests.post(url,json=request_message)
 print(response.status_code)
-print(response.json())
+print(response.json()[0]['output'])
