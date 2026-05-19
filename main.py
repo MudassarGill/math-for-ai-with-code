ca897e56e9241eb53c1b96fd0bbf074c434b16cd
MudassarGill <mudassarhussain6533@gmail.com>
2026-05-18 22:18:01 -0700

    dependency injecction

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from pydantic import BaseModel
-import asyncio
+
+
 app=FastAPI()
 
-@app.get("/users/{user_id}")
-async def get_user(user_id:int):
-    await asyncio.sleep(5)
-    return {"user_id":user_id}
-@app.get("/user_name/{user_name}")
-async def get_user_name(user_name:str):
-    return user_name
+def get_message(msg:str):
+    return 'Hello from dependencyinjection:'
 
-@app.get("/")
-async def read_root():
-    return {'Massege':'FastAPI is alive'}
-
-@app.get('/users')
-async def read_users(skip:int=0,limit:int=10):
-    return {'message':f'we have {limit} users starting from {skip}'}
+@app.get('/')
+def users(msg = Depends(get_message)):
+    return f'{msg}'
 
 
-class user(BaseModel):
-    name:str
-    age:int
-    email:str
-   
 
-@app.post('/users')
-async def create_user(user:user):
-    return {'message':f'User {user.name} created'}
-    
